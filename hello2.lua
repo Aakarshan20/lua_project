eb32e19b3db3dfa65d06fe59bfaa0f482d961c50
iiooiio <iiooiio@hotmail.com>
2022-03-04 01:33:26 -0800

    first commit

---
+++ hello2.lua
@@ -0,0 +1,13 @@
+function fun()
+	a=10
+	local b=20
+	print(b)
+end
+
+fun()
+
+print(type(a),b)
+print(type(true))
+print(type(fun))
+print(type("hello world"))
+-- print(b)
---
+++ hello3.lua
@@ -0,0 +1,28 @@
+str="hello".."world"
+
+print(str)
+
+a,b = 10,20
+
+print(a,b)
+
+c,d,e = 1,2
+
+print(c,d,e)
+
+x,y = 4,5,6
+
+print(x,y)
+
+print(5/0)
+
+print(10*"ohhohoh")
+
+
+--[[
+eval "$(ssh-agent -s)" 
+$ ssh-add ~/.ssh/github_key
+
+
+cat ~/.ssh/github_key.pub 
+---]]
